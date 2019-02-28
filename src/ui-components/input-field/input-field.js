2c96139c7eba1cebc879b754dbceca7cfe6170e2
Jurate Pakalnyte <jurate.pakalnyte@relativity.com>
2019-02-28 16:27:23 +0200

    started input field implementation

---
+++ src/ui-components/input-field/input-field.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./input-field.scss"
+
+function InputField({ }) {
+    return (
+        <div>
+            <input label="label" placeholder="Placeholder"></input>
+            <label></label>
+        </div>
+    )
+}
+
+export default InputField;
