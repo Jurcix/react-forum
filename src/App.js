65f98e0935a9d0719a66c3b98ea0371822f4dab8
Jurcix <pakalnyte.jurate@gmail.com>
2019-03-03 18:53:40 +0200

    implemented input field component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,16 @@ class App extends Component {
   render() {
     return (
       <div>
-        <InputField />
+        <InputField
+          id="thisisid"
+          type="text"
+          placeholder="Placeholder"
+          required={false}
+          value="hello"
+          label="Label"
+          validationMessages={["this is validation message what happens if its a very super duper long validation message", "another message"]}
+          handleChange={(event) => console.log(event.target.value)}
+         />
       </div>
     );
   }
--- src/ui-components/input-field/input-field.js
+++ src/ui-components/input-field/input-field.js
@@ -1,13 +1,73 @@
-import React from "react";
-import "./input-field.scss"
+import React, {Component} from "react";
+import "./input-field.scss";
 
-function InputField({ }) {
+class InputField extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isInputFieldFocused: false,
+      value: props.value || ''
+    };
+  }
+
+  _handleChange(event) {
+    this.setState({ value: event.target.value })
+    event.stopPropagation();
+  }
+
+  _handleFocus(event) {
+    this.setState({ isInputFieldFocused: true });
+    event.stopPropagation();
+  }
+
+  _handleBlur(event) {
+    this.setState({ isInputFieldFocused: false });
+    event.stopPropagation();
+  }
+
+  _getLabelClassName() {
+    let className = "empty-field";
+    if (this.state.value) {
+      className = "non-empty-field"
+    }
+    if (this.state.isInputFieldFocused) {
+      className = "focused"
+    }
+
+    return className;
+  }
+
+  render() {
+    const {
+      id,
+      type,
+      required,
+      label,
+      validationMessages = [],
+    } = this.props;
+
     return (
-        <div>
-            <input label="label" placeholder="Placeholder"></input>
-            <label></label>
-        </div>
+      <div className="input-field">
+        <label className={this._getLabelClassName()} htmlFor={id}> {label} </label>
+        <input
+          id={id}
+          type={type}
+          required={required}
+          onChange={(event) => this._handleChange(event)}
+          onFocus={(event) => this._handleFocus(event)}
+          onBlur={(event) => this._handleBlur(event)}
+          value={this.state.value}
+        />
+        {validationMessages.length > 0 &&
+          validationMessages.map(
+            (message, key) => (
+                <p className="input-field__validation-message" key={key}> {message} </p>
+            )
+          )
+        }
+      </div>
     )
+  }
 }
 
 export default InputField;
